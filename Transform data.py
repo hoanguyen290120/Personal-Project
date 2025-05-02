d1adb46562504950a8d99908f77727554ffe9ac0
hoanguyen290120 <hoanguyen290120@gmail.com>
2025-05-02 14:46:51 +0700

    Update Transform data.py

--- Transform data.py
+++ Transform data.py
@@ -9,7 +9,7 @@ spark = SparkSession.builder \
     .config("spark.driver.extraClassPath", "E:\\System\\Spark\\spark-3.5.5-bin-hadoop3\\jars\\mssql-jdbc-12.10.0.jre11.jar") \
     .getOrCreate()
 
-# 2. Đọc tất cả các file CSV trong 4 thư mục con
+# 2. Đọc tất cả các file CSV
 base_path = r"C:\Users\Admin\PycharmProjects\PythonProject1\Data\archive"
 subfolders = ["Application", "Purchasing", "Sales", "Warehouse"]
 all_dataframes = {}
@@ -19,15 +19,15 @@ for folder in subfolders:
     for file in os.listdir(folder_path):
         if file.endswith(".csv"):
             file_path = os.path.join(folder_path, file)
-            table_name = file.replace(".csv", "").replace(".", "_")  # Tên bảng sạch
-            df = spark.read.option("header", True).option("delimiter", ";").csv(file_path)  # Đọc file CSV với delimiter ";"
+            table_name = file.replace(".csv", "").replace(".", "_")  
+            df = spark.read.option("header", True).option("delimiter", ";").csv(file_path) 
             all_dataframes[table_name] = df
-            print(f"✅ Đã đọc file: {file_path}")
+            
 
 # 3. Cấu hình kết nối SQL Server
-database = "WideWorldImporters_dbt"
-user = "vanhoa290120"
-password = "Hoa0906598967**"
+database = "WideWorldImporters"
+user = "*******"
+password = "*******"
 driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 url = (
     f"jdbc:sqlserver://localhost:1433;databaseName={database};"
@@ -40,19 +40,19 @@ jdbc_properties = {
     "driver": driver
 }
 
-# 4. Ghi từng DataFrame vào SQL Server (Sử dụng schema.table để xác định tên bảng)
+# 4. Ghi từng DataFrame vào SQL Server 
 for table_name, df in all_dataframes.items():
-    schema_table = table_name.split('.')  # Tách schema và table từ tên
-    if len(schema_table) == 2:  # Nếu có schema
+    schema_table = table_name.split('.')  
+    if len(schema_table) == 2: 
         schema = schema_table[0]
         table = schema_table[1]
-        full_table_name = f"{schema}.{table}"  # Tên đầy đủ của bảng (schema.table)
-        print(f"🚀 Đang ghi bảng: {full_table_name}")
+        full_table_name = f"{schema}.{table}"  
+        print(f" Đang ghi bảng: {full_table_name}")
         df.write.mode("overwrite").jdbc(url=url, table=full_table_name, properties=jdbc_properties)
-        print(f"✅ Đã ghi xong bảng: {full_table_name}")
+        print(f"Đã ghi xong bảng: {full_table_name}")
     else:
-        # Trường hợp không có schema (chỉ có table)
+       
         table = table_name
-        print(f"🚀 Đang ghi bảng: {table}")
+        print(f" Đang ghi bảng: {table}")
         df.write.mode("overwrite").jdbc(url=url, table=table, properties=jdbc_properties)
-        print(f"✅ Đã ghi xong bảng: {table}")
+        print(f" Đã ghi xong bảng: {table}")
